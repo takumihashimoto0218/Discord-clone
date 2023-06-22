befe4de4b63e7203d7e7918d20d58cabe5f1eeb7
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-23 02:52:22 +0900

    add画像を入れてcssを整える

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Sidebar.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
 
 const Sidebar = () => {
   return (
@@ -17,6 +18,16 @@ const Sidebar = () => {
         <div className="sidebarTop">
           <h3>Discord</h3>
           <ExpandMoreIcon />
+        </div>
+
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+            <ExpandMoreIcon /> 
+            <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
         </div>
       </div>
     </div>
