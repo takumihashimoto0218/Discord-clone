99ee22a97f361deb749dd85cd794e26c6b3f2044
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-23 01:09:07 +0900

    Sidebarコンポーネント作成

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className="sisdebar">
+      <div className="sidebarLeft"></div>
+      <div className="sidebarRight"></div>
+    </div>
+  )
+}
+
+export default Sidebar
