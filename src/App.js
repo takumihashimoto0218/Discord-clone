01f6d9c9aaa5400843cad932b861999e25608447
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-24 00:04:28 +0900

    chatコンポーネントを作成する

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/chat/Chat';
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
-
+      <Chat />
     </div>
   );
 }
